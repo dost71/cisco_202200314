cddbb72ec15fad9d9f458dc0fe28074d5c7a7410
dost71 <101890520+dost71@users.noreply.github.com>
2022-03-18 21:39:07 +0300

    Update egzersiz1.py

---
+++ Egzersiz/Erdal/egzersiz1.py
@@ -0,0 +1,11 @@
+class cokgen:
+    def __init__(self,cokgenadi,kenarsayisi,acılistesi):
+        self.cokgenadi = cokgenadi
+        self.kenarsayisi = kenarsayisi
+        self.acılistesi = acılistesi
+    def cokgenadisoyle(self):
+        print("Çokgen Adi",self.cokgenadi,"Kenar Sayısı", self.kenarsayisi, "Açı Listesi",self.acılistesi,"olan bir çokgendir.")
+cokgen1 = cokgen("Üçgen","3",[30,60,90])
+cokgen2 = cokgen("Kare","4",[90,90,90,90])
+cokgen1.cokgenadisoyle()
+cokgen2.cokgenadisoyle()
